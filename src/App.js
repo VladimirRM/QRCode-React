ac75cbfdaaadd1c70fed623e5ec09f67870b89ac
Vladimir RM <shvdrummer@gmail.com>
2022-11-10 15:43:51 +0200

    add const const generateQrCode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [qrText, setQrText] = useState('')
+const [qrCode, setQrCode] = useState('')
+
+const generateQrCode =()=>{
+  QRCode.ToDataUrl(
+    qrText,{
+      width:900,
+      height: 3,
+    },
+    (err,url){
+      if(err) return console.log(err)
+      setQrCode(url)
+    }
+  )
+}
+
+
+
+
+
   return (
     <div className="App">
  
--- src/UI/basic-input.js
+++ src/UI/basic-input.js
@@ -3,7 +3,11 @@ import React from 'react'
 
 const BasicInput = ({value,type,onChange,...rest}) => {
   return (
-    <div></div>
+    <div {...rest}>
+        <label>{label}</label>
+        <br />
+        <input type={type} value={value} onChange={onChange}  />
+    </div>
   )
 }
 
